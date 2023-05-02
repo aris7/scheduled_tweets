0083229268a559dbe857713e46abe8685c73bbf4
Aris Krasniqi <aris.krasniqi@webit.de>
2023-05-02 10:31:06 +0200

    add edit and delete tweets

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,5 +1,15 @@
 class TwitterAccount < ApplicationRecord
   belongs_to :user
+  has_many :tweets
 
   validates :username, uniqueness: true
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = "Rails.application.credentials.dig(:twitter, :api_key)"
+      config.consumer_token = "Rails.application.credentials.dig(:twitter, :api_secret) "
+      config.access_token = ""
+      config.access_token_secret = ""
+    end
+  end
 end
